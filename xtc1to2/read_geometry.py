85976570ca7ac3888429dac20f31f5ae4a35e65f
monarin <monarin@gmail.com>
2022-02-15 09:50:22 -0800

    add geometry reader for pixel_position, etc.

---
+++ xtc1to2/read_geometry.py
@@ -0,0 +1,45 @@
+from PSCalib.GeometryAccess import GeometryAccess
+import numpy as np
+
+class PsanaGeometry:
+    """A getter that reads in lcls1-style geometry file.
+
+    Use this access info from  geometry file (*-end.data). 
+    Available info is set as class attributes.
+    """
+
+    pixel_position = None
+    pixel_index_map= None
+
+    def __init__(self, geom):
+        """Sets coordinate in real space (convert to m)."""
+        cframe = 0      # fixed to psana style (1 is for lab conventions)
+        geometry = GeometryAccess(geom, cframe=cframe)
+
+        # Stores a tuple of x,y, and z coordinate arrays
+        pixel_coords = geometry.get_pixel_coords(cframe=cframe)
+        pixel_coord_indexes = geometry.get_pixel_coord_indexes(cframe=cframe)
+        
+        # Converts to metre unit for pixel coordinates 
+        temp = [np.asarray(t) * 1e-6 for t in pixel_coords]
+        temp_index = [np.asarray(t) for t in pixel_coord_indexes]
+
+        # The shape of each axis is represented by five numbers (for this det)
+        # e.g. (1,2,2,512,512). We calculate no. of panels by multiplying
+        # all numbers except the last two (#pixel_x, #pixel_y).
+        panel_num = np.prod(temp[0].shape[:-2])
+
+        shape = (panel_num, temp[0].shape[-2], temp[0].shape[-1])
+        pixel_position = np.zeros(shape + (3,))     # x,y,z
+        pixel_index_map = np.zeros(shape + (2,))    # x,y
+
+        for n in range(3):
+            pixel_position[..., n] = temp[n].reshape(shape)
+
+        for n in range(2):
+            pixel_index_map[..., n] = temp_index[n].reshape(shape)
+        pixel_index_map = pixel_index_map.astype(np.int64)
+           
+        self.pixel_position = pixel_position
+        self.pixel_index_map = pixel_index_map
+
--- xtc1to2/zmq_push.py
+++ xtc1to2/zmq_push.py
@@ -1,14 +1,24 @@
+"""An example that reads in lcls1 experiment and push data to
+be saved in lcls2 format via zmq.
+
+Usage: 
+    1. Get psana1 environment (source /reg/g/psdm/etc/psconda.sh) and 
+    activate py3 env. (conda activate ana-4.0.36-py3)
+    2. Set exp, run, mode, and detector name
+    3. Set path to geometry file 
+"""
 from read_img import PsanaImg, DisplaySPIImg
 from read_photon_energy import PsanaPhotonEnergy
+from read_geometry import PsanaGeometry
 
 # Specify the dataset and detector...
 exp, run, mode, detector_name = 'amo06516', '90', 'idx', 'pnccdFront'
+geom = "/reg/d/psdm/amo/amo06516/calib/PNCCD::CalibV1/Camp.0:pnCCD.0/geometry/38-end.data"
 
-# Initialize an image reader...
+# Initialize all readers
 img_reader = PsanaImg(exp, run, mode, detector_name)
-
-# Initialize photon energy reader
 phe_reader = PsanaPhotonEnergy(exp, run, mode)
+gmt_reader = PsanaGeometry(geom)
 
 # Access an image (e.g. event 796)...
 event_num = 796
@@ -18,5 +28,7 @@ ts = img_reader.timestamp(event_num)
 
 
 print(f'event_num={event_num} ts={ts} {img.shape} photon energy:{photon_energy:.3f}')
+print(f'pixel_position={gmt_reader.pixel_position.shape}')
+print(f'pixel_index_map={gmt_reader.pixel_index_map.shape}')
 
 
