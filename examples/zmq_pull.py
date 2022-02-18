85c608bda1cc4bd4b84c0192c29f375c2f147d1b
monarin <monarin@gmail.com>
2022-02-17 21:40:41 -0800

    add scan data and test

--- examples/zmq_pull.py
+++ examples/zmq_pull.py
@@ -9,103 +9,157 @@ Usage:
 from xtc1to2.socket.zmqhelper import ZmqReceiver
 import dgrampy as dp
 from psana.psexp import TransitionId
+import numpy as np
+import h5py
+from psana import DataSource
 
 
-# NameId setup
-nodeId = 0
-namesId = {
-    "amopnccd": 0,
-    "runinfo": 1,
-}
+def test_output():
+    """Compares known data (saved to hdf5 by the push process) with read data."""
+    f = h5py.File('out.hdf5', 'r')
+    pixel_position = f['pixel_position']
+    pixel_index_map = f['pixel_index_map']
+    data = f['data']
+    photon_energy = f['photon_energy']
+
+    ds = DataSource(files='out.xtc2')
+    run = next(ds.runs())
+    det = run.Detector('amopnccd')
+    pp_det = run.Detector('pixel_position')
+    pim_det = run.Detector('pixel_index_map')
+    data_array = np.zeros([3,4,512,512], dtype=np.float32)
+    photon_array = np.zeros(3, dtype=np.float64)
+    for i,evt in enumerate(run.events()):
+        data_array[i,:,:,:] = det.raw.calib(evt)
+        photon_array[i] = det.raw.photon_energy(evt)
+        pixel_position_array = pp_det(evt)
+        pixel_index_map_array = pim_det(evt)
+
+    assert np.array_equal(data,data_array)
+    assert np.array_equal(photon_energy, photon_array)
+    assert np.array_equal(pixel_position, pixel_position_array)
+    assert np.array_equal(pixel_index_map, pixel_index_map_array)
 
 
-# Setup socket for zmq connection
-socket = "tcp://127.0.0.1:5557"
-zmq_recv = ZmqReceiver(socket)
 
 
-# Create config, algorithm, and detector
-config = dp.config()
-alg = dp.alg("raw", 1, 2, 3)
-det = dp.det("amopnccd", "pnccd", "detnum1234")
+if __name__ == "__main__":
 
-runinfo_alg = dp.alg("runinfo", 0, 0, 1)
-runinfo_det = dp.det("runinfo", "runinfo", "")
-
-
-# Define data formats
-datadef_dict = {
-    "calib": (dp.DataType.FLOAT, 3),
-    "photon_energy": (dp.DataType.DOUBLE, 0),
-}
-datadef = dp.datadef(datadef_dict)
-
-runinfodef_dict = {
-    "expt": (dp.DataType.CHARSTR, 1),
-    "runnum": (dp.DataType.UINT32, 0),
-}
-runinfodef = dp.datadef(runinfodef_dict)
+    # NameId setup
+    nodeId = 1 
+    namesId = {
+        "amopnccd": 0,
+        "runinfo": 1,
+        "scan": 2,
+    }
 
 
-# Create Names
-data_names = dp.names(
-    config, det, alg, datadef, nodeId=nodeId, namesId=namesId["amopnccd"], segment=0
-)
-runinfo_names = dp.names(
-    config,
-    runinfo_det,
-    runinfo_alg,
-    runinfodef,
-    nodeId=nodeId,
-    namesId=namesId["runinfo"],
-    segment=0,
-)
+    # Setup socket for zmq connection
+    socket = "tcp://127.0.0.1:5557"
+    zmq_recv = ZmqReceiver(socket)
 
 
-# Start saving data
-while True:
-    obj = zmq_recv.recv_zipped_pickle()
+    # Create config, algorithm, and detector
+    config = dp.config()
+    alg = dp.alg("raw", 1, 2, 3)
+    det = dp.det("amopnccd", "pnccd", "detnum1234")
 
-    # Begin timestamp is needed (we calculate this from the first L1Accept)
-    # to set the correct timestamp for all transitions prior to the first L1.
-    if "start" in obj:
-        config_timestamp = obj["config_timestamp"]
-        dp.updatetimestamp(config, config_timestamp)
-        dp.save(config)
+    runinfo_alg = dp.alg("runinfo", 0, 0, 1)
+    runinfo_det = dp.det("runinfo", "runinfo", "")
 
-        beginrun = dp.dgram(transid=TransitionId.BeginRun, ts=config_timestamp + 1)
-        beginrun_data = {"expt": "amo06516", "runnum": 90}
-        dp.adddata(beginrun, runinfo_names, runinfodef, beginrun_data)
-        dp.save(beginrun)
-
-        beginstep = dp.dgram(transid=TransitionId.BeginStep, ts=config_timestamp + 2)
-        dp.save(beginstep)
-
-        enable = dp.dgram(transid=TransitionId.Enable, ts=config_timestamp + 3)
-        dp.save(enable)
-    elif "end" in obj:
-        disable = dp.dgram(transid=TransitionId.Disable, ts=current_timestamp + 1)
-        dp.save(disable)
-
-        endstep = dp.dgram(transid=TransitionId.EndStep, ts=current_timestamp + 2)
-        dp.save(disable)
-
-        endrun = dp.dgram(transid=TransitionId.EndRun, ts=current_timestamp + 3)
-        dp.save(endrun)
-
-        break
-    else:
-        # Create L1Accept
-        d0 = dp.dgram(ts=obj["timestamp"])
-        data = {
-            "calib": obj["calib"],
-            "photon_energy": obj["photon_energy"],
-        }
-        dp.adddata(d0, data_names, datadef, data)
-        dp.save(d0)
-        current_timestamp = obj["timestamp"]
+    scan_alg = dp.alg("raw", 2, 0, 0)
+    scan_det = dp.det("scan", "scan", "detnum1234")
 
 
-# Write out dgrampy buffer to xtc2 file
-with open("out.xtc2", "wb") as f:
-    f.write(dp.get_buf())
+    # Define data formats
+    datadef_dict = {
+        "calib": (dp.DataType.FLOAT, 3),
+        "photon_energy": (dp.DataType.DOUBLE, 0),
+    }
+    datadef = dp.datadef(datadef_dict)
+
+    runinfodef_dict = {
+        "expt": (dp.DataType.CHARSTR, 1),
+        "runnum": (dp.DataType.UINT32, 0),
+    }
+    runinfodef = dp.datadef(runinfodef_dict)
+
+    scandef_dict = {
+        "pixel_position": (dp.DataType.DOUBLE, 4),
+        "pixel_index_map": (dp.DataType.INT64, 4),
+    }
+    scandef = dp.datadef(scandef_dict)
+
+
+    # Create Names
+    data_names = dp.names(
+        config, det, alg, datadef, nodeId=nodeId, namesId=namesId["amopnccd"], segment=0
+    )
+    runinfo_names = dp.names(
+        config,
+        runinfo_det,
+        runinfo_alg,
+        runinfodef,
+        nodeId=nodeId,
+        namesId=namesId["runinfo"],
+        segment=0,
+    )
+    scan_names = dp.names(
+        config, scan_det, scan_alg, scandef, nodeId=nodeId, namesId=namesId["scan"], segment=0
+    )
+
+
+    # Start saving data
+    while True:
+        obj = zmq_recv.recv_zipped_pickle()
+
+        # Begin timestamp is needed (we calculate this from the first L1Accept)
+        # to set the correct timestamp for all transitions prior to the first L1.
+        if "start" in obj:
+            config_timestamp = obj["config_timestamp"]
+            dp.updatetimestamp(config, config_timestamp)
+            dp.save(config)
+
+            beginrun = dp.dgram(transid=TransitionId.BeginRun, ts=config_timestamp + 1)
+            beginrun_data = {"expt": "amo06516", "runnum": 90}
+            dp.adddata(beginrun, runinfo_names, runinfodef, beginrun_data)
+            dp.save(beginrun)
+            
+            beginstep = dp.dgram(transid=TransitionId.BeginStep, ts=config_timestamp + 2)
+            scan_data = {
+                'pixel_position': obj['pixel_position'],
+                'pixel_index_map': obj['pixel_index_map']
+            }
+            dp.adddata(beginstep, scan_names, scandef, scan_data)
+            dp.save(beginstep)
+            
+            enable = dp.dgram(transid=TransitionId.Enable, ts=config_timestamp + 3)
+            dp.save(enable)
+            current_timestamp = config_timestamp + 3
+
+        elif "end" in obj:
+            disable = dp.dgram(transid=TransitionId.Disable, ts=current_timestamp + 1)
+            dp.save(disable)
+            endstep = dp.dgram(transid=TransitionId.EndStep, ts=current_timestamp + 2)
+            dp.save(disable)
+            endrun = dp.dgram(transid=TransitionId.EndRun, ts=current_timestamp + 3)
+            dp.save(endrun)
+            break
+
+        else:
+            # Create L1Accept
+            d0 = dp.dgram(ts=obj["timestamp"])
+            data = {
+                "calib": obj["calib"],
+                "photon_energy": obj["photon_energy"],
+            }
+            dp.adddata(d0, data_names, datadef, data)
+            dp.save(d0)
+            current_timestamp = obj["timestamp"]
+
+
+    # Write out dgrampy buffer to xtc2 file
+    with open("out.xtc2", "wb") as f:
+        f.write(dp.get_buf())
+
+    test_output()
--- examples/zmq_push.py
+++ examples/zmq_push.py
@@ -11,6 +11,9 @@ from xtc1to2.read_img import PsanaImg, DisplaySPIImg
 from xtc1to2.read_photon_energy import PsanaPhotonEnergy
 from xtc1to2.read_geometry import PsanaGeometry
 from xtc1to2.socket.zmqhelper import ZmqSender
+import h5py
+import numpy as np
+
 
 # Specify the dataset and detector...
 exp, run, mode, detector_name = "amo06516", "90", "idx", "pnccdFront"
@@ -29,29 +32,37 @@ zmq_send = ZmqSender(socket)
 
 # Access an image (e.g. event 796)...
 start_evt = 796
-for event_num in range(start_evt, start_evt + 3):
+data_array = np.zeros([3,4,512,512], dtype=np.float32)
+photon_array = np.zeros(3, dtype=np.float64)
+for i, event_num in enumerate(range(start_evt, start_evt + 3)):
     img = img_reader.get(event_num, calib=True)
     photon_energy = phe_reader.get(event_num)
     ts = img_reader.timestamp(event_num)
+    data_array[i,:,:,:] = img
+    photon_array[i] = photon_energy
 
     if event_num == start_evt:
-        # Send beginning timestamp
-        start_dict = {"start": True, "config_timestamp": ts.time() - 10}
+        # Send beginning timestamp - this will create config, beginrun,
+        # beginstep, and enable on the client.
+        start_dict = {
+            "start": True, 
+            "config_timestamp": ts.time() - 10,
+            "pixel_position": gmt_reader.pixel_position,
+            "pixel_index_map": gmt_reader.pixel_index_map,
+        }
         zmq_send.send_zipped_pickle(start_dict)
-
+        
     data = {
         "calib": img,
         "photon_energy": photon_energy,
         "timestamp": ts.time(),
-        "pixel_position": gmt_reader.pixel_position,
-        "pixel_index_map": gmt_reader.pixel_index_map,
     }
 
     print(
-        f"event_num={event_num} ts={ts.time()} {img.shape} photon energy:{photon_energy:.3f}"
+        f"event_num={event_num} ts={ts.time()} img={img.shape} dtype={img.dtype} photon energy:{photon_energy:.3f}"
     )
-    print(f"pixel_position={gmt_reader.pixel_position.shape}")
-    print(f"pixel_index_map={gmt_reader.pixel_index_map.shape}")
+    print(f"pixel_position={gmt_reader.pixel_position.shape} dtype={gmt_reader.pixel_position.dtype}")
+    print(f"pixel_index_map={gmt_reader.pixel_index_map.shape} dtype={gmt_reader.pixel_index_map.dtype}")
 
     # Send the dataset
     zmq_send.send_zipped_pickle(data)
@@ -59,3 +70,11 @@ for event_num in range(start_evt, start_evt + 3):
 # Send end message
 done_dict = {"end": True}
 zmq_send.send_zipped_pickle(done_dict)
+
+print(f'photon_array={photon_array}')
+with h5py.File("out.hdf5", "w") as f:
+    f.create_dataset("pixel_position", data=gmt_reader.pixel_position)
+    f.create_dataset("pixel_index_map", data=gmt_reader.pixel_index_map)
+    f.create_dataset("data", data=data_array)
+    f.create_dataset("photon_energy", data=photon_array)
+
