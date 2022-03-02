8ca717c8b9a4ba8aafbadcb835d57a27094a6793
monarin <monarin@gmail.com>
2022-03-02 15:51:43 -0800

    add ideal-world spinifel example

--- examples/analyze.py
+++ examples/analyze.py
@@ -2,21 +2,35 @@ from psana import DataSource
 
 
 # Start datasource
-xtc_dir= '/cds/home/m/monarin/tmp/amo06516'
-ds = DataSource(exp='amo06516', run=85, dir=xtc_dir)
+xtc_dir = "/cds/home/m/monarin/tmp/amo06516"
+ds = DataSource(exp="amo06516", run=85, dir=xtc_dir)
 run = next(ds.runs())
 
 
 # Obtain detectors
-det = run.Detector('amopnccd')
-pp_det = run.Detector('pixel_position')
-pim_det = run.Detector('pixel_index_map')
+det = run.Detector("amopnccd")
+pp_det = run.Detector("pixel_position")
+pim_det = run.Detector("pixel_index_map")
 
 
 # Access events
-for evt in run.events():
-    calib = det.raw.calib(evt)
+for nevt, evt in enumerate(run.events()):
+    # Save images in a list
+    calibs.append(det.raw.calib(evt))
+
+    # Per run variables
     photon_energy = det.raw.photon_energy(evt)
     pixel_position = pp_det(evt)
     pixel_index_map = pim_det(evt)
-    print(evt.timestamp, photon_energy, calib.shape, pixel_position.shape, pixel_index_map.shape)
+
+    # Ideal world spinifel
+    if nevt == N_images_per_rank:
+        solve_ac(calibs, photon_energy, pixel_position, pixel_index_map)
+
+    print(
+        evt.timestamp,
+        photon_energy,
+        calib.shape,
+        pixel_position.shape,
+        pixel_index_map.shape,
+    )
