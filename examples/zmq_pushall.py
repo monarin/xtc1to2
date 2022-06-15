310d87ea5de981fa9e2f031092808d41cbba5ae7
monarin <monarin@gmail.com>
2022-06-15 14:42:18 -0700

    make sure we don't write special transitions when many runs are combined into one

--- examples/zmq_pushall.py
+++ examples/zmq_pushall.py
@@ -17,88 +17,110 @@ import h5py
 import numpy as np
 import csv
 
+expected_label1_events = 1669
+def get_run_info():
+    """Returns a dictionary of run and selected events.
+    
+    Open csv with each line containing 'exp', 'run', 'event_num', 'label'
+    Total evt: 5059 - 1 (header) = 5058
+    label 0: 2697
+    label 1: 1669
+    label 2: 681
+    """
+    fl_csv     = "2022_0603_2226_44.auto.label.csv"
+    label_1hit = '1'
 
-fl_csv     = "2022_0603_2226_44.auto.label.csv"
-label_1hit = '1'
+    # This is a dictionary with a key as run number. Each pointing
+    # to a list of event number that will be selected.
+    run_info = {}
+    with open(fl_csv) as csv_handle:
+        lines = csv.reader(csv_handle)
+        
+        #skip header
+        next(lines)
+        for line in lines:
+            exp, run, evt_no, label = line
+            if label != label_1hit: continue
+            run = int(run)
+            if run in run_info:
+                run_info[run].append(int(evt_no))
+            else:
+                run_info[run] = [int(evt_no)]
 
-# Specify the dataset and detector...
-## exp, run, mode, detector_name = "amo06516", "90", "idx", "pnccdFront"
-geom = (
-    "/reg/d/psdm/amo/amo06516/calib/PNCCD::CalibV1/Camp.0:pnCCD.0/geometry/38-end.data"
-)
-# Initialize the geometry
-gmt_reader = PsanaGeometry(geom)
+    # Check run_info
+    cn_label1 = 0
+    for run, evt_list in run_info.items():
+        n_events = len(evt_list)
+        print(run, n_events)
+        cn_label1 += n_events
+    print(f'Total label 1 events: {cn_label1}')
+    assert cn_label1 == expected_label1_events
+    return run_info
 
-# Initialize zmq sender
-socket = "tcp://127.0.0.1:5558"
-zmq_send = ZmqSender(socket)
 
-# Collect all items from the auto-labeled file...
-mode            = "idx" 
-detector_name   = "pnccdFront"
-event_dict      = {}
-img_reader_dict = {}
-phe_reader_dict = {}
-with open(fl_csv) as csv_handle:
-    lines = csv.reader(csv_handle)
+if __name__ == "__main__":
+    # Specify the dataset and detector...
+    exp, mode, detector_name = "amo06516", "idx", "pnccdFront"
+    geom = (
+        "/reg/d/psdm/amo/amo06516/calib/PNCCD::CalibV1/Camp.0:pnCCD.0/geometry/38-end.data"
+    )
+    # Initialize the geometry
+    gmt_reader = PsanaGeometry(geom)
 
-    # Skip the header...
-    next(lines)
+    # Initialize zmq sender
+    socket = "tcp://127.0.0.1:5558"
+    zmq_send = ZmqSender(socket)
 
-    # Go through each line...
-    for line in lines:
-        exp, run, event_num, label = line
+    # Get event no. list per run
+    run_info = get_run_info()
+    n_runs = len(run_info.keys())
+    cn_runs = 0
 
-        # Skip if not single hit...
-        if label != label_1hit: continue
+    # Start sending data by looping through runs
+    cn_events = 0
+    for run, event_num_list in run_info.items(): 
+        img_reader = PsanaImg(exp, run, mode, detector_name)
+        phe_reader = PsanaPhotonEnergy(exp, run, mode)
 
-        # Specify the key to dictionaries...
-        basename = (exp, run)
+        for i_evt, event_num in enumerate(event_num_list):
+            img = img_reader.get(event_num, calib=True)
+            photon_energy = phe_reader.get(event_num)
+            ts = img_reader.timestamp(event_num)
 
-        # Collect run info from each exp,run...
-        if not basename in event_dict: event_dict[basename] = [event_num]
-        else                         : event_dict[basename].append(event_num)
-
-        # Collect img_reader from each exp,run...
-        if not basename in img_reader_dict: 
-            img_reader_dict[basename] = PsanaImg(exp, run, mode, detector_name)
-
-        # Collect photon energy from each exp,run...
-        if not basename in phe_reader_dict:
-            phe_reader_dict[basename] = PsanaPhotonEnergy(exp, run, mode)
-
-for i, (basename, event_num_list) in enumerate(event_dict.items()):
-    exp, run = basename
-
-    for event_num in event_num_list:
-        ts = img_reader_dict[basename].timestamp(event_num)
-        if i == 0:
-            start_dict = {
-                "exp"             : exp,
-                "run"             : run,
-                "start"           : True,
-                "config_timestamp": ts.time() - 10,
-                "pixel_position"  : gmt_reader.pixel_position,
-                "pixel_index_map" : gmt_reader.pixel_index_map,
+            if i_evt == 0 and cn_runs == 0:
+                # Send beginning timestamp - this will create config, beginrun,
+                # beginstep, and enable on the client.
+                start_dict = {
+                    "start": True, 
+                    "exp": exp,
+                    "run": run,
+                    "config_timestamp": ts.time() - 10,
+                    "pixel_position": gmt_reader.pixel_position,
+                    "pixel_index_map": gmt_reader.pixel_index_map,
+                }
+                zmq_send.send_zipped_pickle(start_dict)
+                
+            data = {
+                "calib": img,
+                "photon_energy": photon_energy,
+                "timestamp": ts.time(),
             }
-            zmq_send.send_zipped_pickle(start_dict)
 
-        img = img_reader_dict[basename].get(event_num, calib = True)
-        phe = phe_reader_dict[basename].get(event_num)
-        data = {
-            "calib"        : img,
-            "photon_energy": phe,
-            "timestamp"    : ts.time(),
-        }
+            print(
+                f"run={run} event_num={event_num} ts={ts.time()} img={img.shape} dtype={img.dtype} photon energy:{photon_energy:.3f}"
+            )
 
-        print(
-            f"exp={exp} run={int(run):04d} event_num={int(event_num):06d} ts={ts.time()} img={img.shape} dtype={img.dtype} photon energy:{phe:.3f}"
-        )
-        print(f"pixel_position={gmt_reader.pixel_position.shape} dtype={gmt_reader.pixel_position.dtype}")
-        print(f"pixel_index_map={gmt_reader.pixel_index_map.shape} dtype={gmt_reader.pixel_index_map.dtype}")
+            # Send the dataset
+            zmq_send.send_zipped_pickle(data)
+            cn_events += 1
 
-        zmq_send.send_zipped_pickle(data)
+        # end for i_evt, ...
+        cn_runs += 1
 
-# Send end message
-done_dict = {"end": True}
-zmq_send.send_zipped_pickle(done_dict)
+    # end for run, ...
+
+    # Send end message
+    done_dict = {"end": True}
+    zmq_send.send_zipped_pickle(done_dict)
+    
+    print(f"Send total: {cn_events} events in {cn_runs} runs")
